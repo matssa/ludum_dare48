9a153605b42421a4bb7763a6db1bc72afd0866f2
Ruben S. Sevaldson <rubenss@stud.ntnu.no>
2021-04-25 01:31:41 +0200

    Add tile functionality

--- main.go
+++ main.go
@@ -57,6 +57,7 @@ var (
 	}
 	myKeys      = []keyboardKey{}
 	gopherImage *ebiten.Image
+	tiles = make([]*Tile, 0, 0)
 )
 
 type keyboardKey struct {
@@ -72,8 +73,8 @@ type Player struct {
 	// Character position
 	x16  int
 	y16  int
-	vy16 int
-	vx16 int
+	vy16 float64
+	vx16 float64
 }
 
 type Game struct {
@@ -84,9 +85,6 @@ type Game struct {
 	world  *ebiten.Image
 	camera Camera
 
-	// Character position
-	characterX int
-	characterY int
 }
 
 func init() {
@@ -102,8 +100,6 @@ func init() {
 func (g *Game) init() {
 	g.player.x16 = 0
 	g.player.y16 = 100 * 16
-	g.characterX = -240
-	g.characterY = 0
 }
 
 func newGame() *Game {
@@ -113,31 +109,37 @@ func newGame() *Game {
 }
 
 func (p *Player) jump() {
-	p.vy16 = -60
+	p.vy16 = -6
 }
 
 func (p *Player) executeMovement() {
 	if inpututil.IsKeyJustPressed(ebiten.KeyW) {
 		p.jump()
+	} else {
+	    for _, tile := range tiles {
+		    if (tile.PlayerCollide(p)) {
+			    p.vy16 = 0;
+	            }
+	    }
 	}
 	if ebiten.IsKeyPressed(ebiten.KeyA) {
 		if !p.looksLeft {
 			p.looksLeft = true
 			p.hasTurned = true
 		}
-		p.vx16 = -40
+		p.vx16 = -5
 	} else if ebiten.IsKeyPressed(ebiten.KeyD) {
 		if p.looksLeft {
 			p.looksLeft = false
 			p.hasTurned = true
 		}
-		p.vx16 = 40
+		p.vx16 = 5
 	} else {
 		p.vx16 = 0
 	}
 
-	p.y16 += p.vy16
-	p.x16 += p.vx16
+	p.y16 += int(p.vy16)
+	p.x16 += int(p.vx16)
 	p.count++
 }
 
@@ -148,9 +150,9 @@ func (g *Game) Update() error {
 	switch g.gameMode {
 	case play:
 		// Gravity
-		g.player.vy16 += 4
-		if g.player.vy16 > 96 {
-			g.player.vy16 = 96
+		g.player.vy16 += 0.3
+		if g.player.vy16 > 10 {
+			g.player.vy16 = 10
 		}
 
 		g.player.executeMovement()
@@ -167,7 +169,7 @@ func (g *Game) drawCharacter() {
 		op.GeoM.Scale(-1, 1)
 		op.GeoM.Translate(float64(animatedSprite.frameWidth), 0)
 	}
-	op.GeoM.Translate(float64(g.player.x16/16.0)-float64(g.characterX), float64(g.player.y16/16.0)-float64(g.characterY))
+	op.GeoM.Translate(float64(g.player.x16), float64(g.player.y16))
 	op.Filter = ebiten.FilterLinear
 	if g.player.count >= 5 {
 		g.player.count = 0
@@ -186,6 +188,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 	if g.gameMode == 1 {
 		// g.drawCharacter()
 		DrawOverlay(screen, 5)
+		for _, tile := range tiles {
+			tile.DrawTile(screen)
+		}
 	}
 
 }
@@ -219,8 +224,33 @@ func main() {
 		5,
 		runnerImage)
 
+
+
+	// Create some tiles
+	posx := 0
+	for i := 0; i < 10; i++ {
+		tiles = append(tiles, NewTile(posx, 100, "top"))
+		posx += 16;
+	}
+	tiles = append(tiles, NewTile(posx, 100, "top-right"))
+	posx = 64
+	tiles = append(tiles, NewTile(posx, 150, "top-left"))
+	for i := 0; i < 20; i++ {
+		posx += 16;
+		tiles = append(tiles, NewTile(posx, 150, "top"))
+	}
+	posx = 0
+	for i := 0; i < 10; i++ {
+		tiles = append(tiles, NewTile(posx, 200, "top"))
+		posx += 16;
+	}
+	tiles = append(tiles, NewTile(posx, 200, "top-right"))
+
+
+
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
 	ebiten.SetWindowTitle("Animation (Ebiten Demo)")
+
 	newGame()
 
 	if err := ebiten.RunGame(g); err != nil {
---
+++ tile.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+
+type Tile struct {
+	posx int
+	posy int
+	sprite_selector string
+
+	open_image *ebiten.Image // PRIVATE FIELD
+}
+
+func NewTile(posx int, posy int, sprite_selector string) *Tile {
+	tileimage, err := ebitenutil.OpenFile("./resources/sprites/tile-map.png");
+	if err != nil {
+		log.Fatal(err)
+	}
+	img, _, err := image.Decode(tileimage)
+	if err != nil {
+		log.Fatal(err)
+	}
+	tileImage := ebiten.NewImageFromImage(img)
+
+	return &Tile{
+		posx:   posx,
+		posy:   posy,
+		sprite_selector: sprite_selector,
+		open_image: tileImage,
+	}
+}
+
+func (t Tile) DrawTile(screen *ebiten.Image) {
+	var startx int
+	var starty int
+	switch t.sprite_selector {
+	case "top-left":
+		startx = 0
+		starty = 0
+	case "top":
+		startx = 16
+		starty = 0
+	case "top-right":
+		startx = 32
+		starty = 0
+	default:
+		log.Fatal("sprite selector not implemented yet");
+	}
+
+	subimg := t.open_image.SubImage(image.Rect(
+		startx,
+		starty,
+		startx + 16,
+		starty + 16)).(*ebiten.Image)
+
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(float64(t.posx), float64(t.posy))
+	screen.DrawImage(subimg, op)
+}
+
+func (tile Tile) PlayerCollide(p *Player) bool {
+		    isInX1 := p.x16 >= tile.posx && p.x16 <= tile.posx + 16
+		    isInX2 := p.x16 + 16 >= tile.posx && p.x16 + 16 <= tile.posx + 16
+		    isInY1 := p.y16 >= tile.posy - 8 && p.y16 <= tile.posy + 8
+		    isInY2 := p.y16 + 16 >= tile.posy - 8 && p.y16 + 16 <= tile.posy + 8
+		    return (isInX1 && isInY1) || (isInX2 && isInY2)
+}
