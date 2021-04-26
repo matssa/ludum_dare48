9649fc5e26add6848d8b242b385bf6977f680730
Ruben S. Sevaldson <rubenss@stud.ntnu.no>
2021-04-26 21:48:40 +0200

    Improve platform generation

--- platform-generation.go
+++ platform-generation.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"math"
 	"math/rand"
 	"time"
 )
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+func roundToTileSize(num int) int {
+	return int(math.Round(float64(num)/16.0) * 16)
 }
 
 func createLine(length int, startX int, startY int) []*Tile {
@@ -36,9 +42,10 @@ func createLine(length int, startX int, startY int) []*Tile {
 func createMap() [][]*Tile {
 	firstLineX := 500
 	firstLineY := 1200
+	firstLineLength := 10
 	lines := make([][]*Tile, 0, 0)
 	startLine := make([]*Tile, 0, 0)
-	for i := 0; i < 10; i++ {
+	for i := 0; i < firstLineLength; i++ {
 		startLine = append(startLine, NewTile(
 			firstLineX+i*16,
 			firstLineY,
@@ -48,20 +55,18 @@ func createMap() [][]*Tile {
 
 	numLines := rand.Intn(200-150) + 150
 
-	lineLengths := make([]int, 0, 0)
-	for i := 0; i < numLines; i++ {
-		lineLengths = append(lineLengths, rand.Intn(10-3)+3)
-	}
 
+	prevLineLength := firstLineLength
 	prevx := firstLineX
 	prevy := firstLineY
 	lineXStarts := make([]int, 0, 0)
 	lineYStarts := make([]int, 0, 0)
 	for i := 0; i < numLines; i++ {
-		maxNewX := (prevx + (lineLengths[i] * 16)) + (3 * 16)
-		minNewX := (prevx + (lineLengths[i] * 16)) - (2 * 16)
-		maxNewY := prevy + (3 * 16)
-		minNewY := prevy - (3 * 16)
+		lineLength := rand.Intn(20-3)+3
+		maxNewX := (prevx + (prevLineLength * 16)) + (5 * 16)
+		minNewX := (prevx + (prevLineLength * 16)) - (3 * 16)
+		maxNewY := prevy + (4 * 16)
+		minNewY := prevy - (4 * 16)
 
 		borderLimit := 300
 
@@ -82,14 +87,38 @@ func createMap() [][]*Tile {
 			newY = rand.Intn(maxNewY-minNewY) + minNewY
 		}
 
+		newX = roundToTileSize(newX);
+		newY = roundToTileSize(newY);
+
 		lineXStarts = append(lineXStarts, newX)
 		lineYStarts = append(lineYStarts, newY)
 		prevx = newX
 		prevy = newY
-	}
+		prevLineLength = lineLength
+		lines = append(lines, createLine(lineLength, newX, newY))
 
-	for i := 0; i < numLines; i++ {
-		lines = append(lines, createLine(lineLengths[i], lineXStarts[i], lineYStarts[i]))
+		extraLines := rand.Intn(5);
+		for i := 0; i < extraLines; i++ {
+			extraLineLength := rand.Intn(25-1)+2
+			extraMaxNewX := (prevx + (extraLineLength * 16)) + (25 * 16)
+			extraMinNewX := (prevx + (extraLineLength * 16)) - (25 * 16)
+			extraMaxNewY := prevy + (25 * 16)
+			extraMinNewY := prevy - (25 * 16)
+
+			extraNewX := rand.Intn(extraMaxNewX-extraMinNewX) + extraMinNewX
+			if extraNewX < borderLimit || extraNewX > worldWidth-borderLimit {
+				fmt.Printf("happened x\n");
+				continue
+			}
+			extraNewY := rand.Intn(extraMaxNewY-extraMinNewY) + extraMinNewY
+			if extraNewY < borderLimit || extraNewY > (worldHeight-400-borderLimit) {
+				fmt.Printf("happened x\n");
+				continue;
+			}
+			extraNewX = roundToTileSize(extraNewX)
+			extraNewY = roundToTileSize(extraNewY)
+			lines = append(lines, createLine(extraLineLength, extraNewX, extraNewY))
+		}
 	}
 
 	return lines
